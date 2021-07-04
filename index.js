bcb37eb7446829505038fb6f51a9a6115e4ef4e4
Henrik-3 <henrikmertens3@gmail.com>
2021-07-04 20:53:35 +0200

    v1.0-b2

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const io = require("socket.io")(5001, {
 const WebSocket = require('ws');
 var wsdata = fetchLogin()
 const ws = new WebSocket(`wss://riot:${wsdata.pw}@127.0.0.1:${wsdata.port}/`, "wamp");
+const settings = fs.readFileSync("settings.json", {encoding: "utf-8"})
 
 if(!fs.existsSync(`${process.env.LOCALAPPDATA}\\Riot Games\\Riot Client\\Config\\lockfile`)) throw "Bitte öffne VALORANT"
 
@@ -72,27 +73,37 @@ var cstate
 
 fastify.get("/v1/core-game", async (req, res) => {
     tokens = tokens == undefined ? await data() : tokens
-    var matchid = await axios.get(`https://glz-eu-1.eu.a.pvp.net/core-game/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-02.09-shipping-14-560778","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
-    console.log(matchid)
+    var matchid = await axios.get(`https://glz-${settings.region}-1.${settings.region}.a.pvp.net/core-game/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    if(matchid.response && matchid.response.status == 400) {
+        tokens = await data()
+        matchid = await axios.get(`https://glz-${settings.region}-1.${settings.region}.a.pvp.net/core-game/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    }
     if(matchid.response) return errorhandler(matchid.response.status, res)
-    var core_game_data = await axios.get(`https://glz-eu-1.eu.a.pvp.net/core-game/v1/matches/${matchid.data.MatchID}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-02.09-shipping-14-560778","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    var core_game_data = await axios.get(`https://glz-${settings.region}-1.${settings.region}.a.pvp.net/core-game/v1/matches/${matchid.data.MatchID}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
     if(core_game_data.response) return errorhandler(core_game_data.response.status, res)
     res.send({data: core_game_data.data, subject: tokens.data.subject})
 })
 
 fastify.get("/v1/pre-game", async (req, res) => {
     tokens = tokens == undefined ? await data() : tokens
-    var matchid = await axios.get(`https://glz-eu-1.eu.a.pvp.net/pregame/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-02.09-shipping-14-560778","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
-    console.log(matchid)
+    var matchid = await axios.get(`https://glz-${settings.region}-1.${settings.region}.a.pvp.net/pregame/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    if(matchid.response && matchid.response.status == 400) {
+        tokens = await data()
+        matchid = await axios.get(`https://glz-${settings.region}-1.${settings.region}.a.pvp.net/pregame/v1/players/${tokens.data.subject}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    }
     if(matchid.response) return errorhandler(matchid.response.status, res)
-    var core_game_data = await axios.get(`https://glz-eu-1.eu.a.pvp.net/pregame/v1/matches/${matchid.data.MatchID}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-02.09-shipping-14-560778","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
-    if(core_game_data.response) return errorhandler(core_game_data.response.status, res)
-    res.send({data: core_game_data.data, subject: tokens.data.subject})
+    var pre_game_data = await axios.get(`https://glz-${settings.region}-1.eu.a.pvp.net/pregame/v1/matches/${matchid.data.MatchID}`, {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    if(pre_game_data.response) return errorhandler(pre_game_data.response.status, res)
+    res.send({data: pre_game_data.data, subject: tokens.data.subject})
 })
 
 fastify.get("/v1/get-name/:id", async (req, res) => {
     var tokens = await data()
-    var playerid = await axios.put(`https://pd.ap.a.pvp.net/name-service/v2/players`, [req.params.id], {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-02.09-shipping-14-560778","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    var playerid = await axios.put(`https://pd.${settings.region}.a.pvp.net/name-service/v2/players`, [req.params.id], {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    if(playerid.response && playerid.response.status == 400) {
+        tokens = await data()
+        playerid = await axios.put(`https://pd.${settings.region}.a.pvp.net/name-service/v2/players`, [req.params.id], {headers: {Authorization: "Bearer " + tokens.data.accessToken,"X-Riot-Entitlements-JWT": tokens.data.token,"X-Riot-ClientVersion": "release-03.00-shipping-22-574489","X-Riot-ClientPlatform": "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9"}}).catch(error => {return error})
+    }
     if(playerid.response) return errorhandler(playerid.response.status, res)
     res.send(playerid.data)
 })
@@ -131,12 +142,10 @@ ws.on('message', async data => {
     var pdata = JSON.parse(data)
     if(typeof pdata[2] == "object" && pdata[2].eventType == "Update" && pdata[2].uri == "/chat/v4/presences" && pdata[2].data.presences[0].puuid == tokens.data.subject) {
         var decode = JSON.parse(atob(pdata[2].data.presences[0].private));
-        console.log(cstate)
         if(cstate != decode.sessionLoopSate) {
-            console.log(decode)
-            if(decode.sessionLoopState == "MENUS") return io.emit("update", {state: "Menu", data: decode}); cstate = decode.sessionLoopState
-            if(decode.sessionLoopState == "PREGAME") return io.emit("update", {state: "PreGame", data: decode}); cstate = decode.sessionLoopState
-            if(decode.sessionLoopState == "INGAME") return io.emit("update", {state: "Ingame", data: decode}); cstate = decode.sessionLoopState
+            if(decode.sessionLoopState == "MENUS") return io.emit("update", {state: "Menu", data: decode}); cstate = decode.sessionLoopState;console.log("State: MENUS")
+            if(decode.sessionLoopState == "PREGAME") return io.emit("update", {state: "PreGame", data: decode}); cstate = decode.sessionLoopState;console.log("State: PREGAME")
+            if(decode.sessionLoopState == "INGAME") return io.emit("update", {state: "Ingame", data: decode}); cstate = decode.sessionLoopState;console.log("State: INGAME")
         }
     }
 });
@@ -157,10 +166,9 @@ io.on("connection", async socket => {
     }).catch(error => {return error})
     var f = presence.data.presences.filter(item => item.puuid == tokens.data.subject)
     var d = JSON.parse(atob(f[0].private))
-    console.log(d)
-    if(d.sessionLoopState == "MENUS") io.emit("initialize", {state: "Menu", data: d})
-    if(d.sessionLoopState == "PREGAME") io.emit("initialize", {state: "PreGame", data: d})
-    if(d.sessionLoopState == "INGAME") io.emit("initialize", {state: "Ingame", data: d})
+    if(d.sessionLoopState == "MENUS") io.emit("initialize", {state: "Menu", data: d});console.log("State: MENUS")
+    if(d.sessionLoopState == "PREGAME") io.emit("initialize", {state: "PreGame", data: d});console.log("State: PREGAME")
+    if(d.sessionLoopState == "INGAME") io.emit("initialize", {state: "Ingame", data: d});console.log("State: INGAME")
     socket.on('disconnect', () => {
         console.log('Disconnected')
     })
