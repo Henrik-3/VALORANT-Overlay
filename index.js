62d449549dda895a533fffc3c392a6e7c1a5deb3
meerpaluten <45709417+meerpaluten@users.noreply.github.com>
2021-08-06 19:49:08 +0200

    That the program still works after a Valorant restart

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const io = require("socket.io")(5001, {
     methods: ["GET", "POST"]
 }});
 const WebSocket = require('ws');
-var wsdata = fetchLogin()
-const ws = new WebSocket(`wss://riot:${wsdata.pw}@127.0.0.1:${wsdata.port}/`, "wamp");
+var wsdata
+websocketConnect()
 const settings = JSON.parse(fs.readFileSync("./settings.json", {encoding: "utf-8"}))
-
-if(!fs.existsSync(`${process.env.LOCALAPPDATA}\\Riot Games\\Riot Client\\Config\\lockfile`)) throw "Bitte öffne VALORANT"
 
 setInterval(async () => {
     tokens = await data()
@@ -39,8 +37,78 @@ var data = async function () {
             })
         })
     } else {
-        throw "VALORANT nicht offen"
+        console.log("Valorant ist nicht geöffnet, versuche es in 10 Sekunden erneut ...")
+        sleep(10000)
+        return await data()
     }
+}
+
+async function websocketConnect() {
+    wsdata = await fetchLogin();
+    var ws = new WebSocket(`wss://riot:${wsdata.pw}@127.0.0.1:${wsdata.port}/`, "wamp");
+    ws.on("open", async open => {
+        tokens = tokens == undefined ? await data() : tokens
+        var presence = await axios.get(`https://127.0.0.1:${wsdata.port}/chat/v4/presences`, {
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`riot:${wsdata.pw}`, 'utf8').toString('base64')}`,
+                "user-agent": "ShooterGame/21 Windows/10.0.19042.1.768.64bit",
+                "X-Riot-ClientVersion": "release-02.03-shipping-8-521855",
+                "Content-Type": "application/json",
+                "rchat-blocking": "true"
+            },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            })
+        }).catch(error => {return error})
+
+        if(presence instanceof Error){
+            console.log("Eine abfrage an Valorant hat ein Fehler geworfen.")
+            ws.close()
+            return
+        }
+
+        try {
+            var f = presence.data.presences.filter(item => item.puuid == tokens.data.subject)
+            var d = JSON.parse(Buffer.from(f[0].private, "base64").toString("utf-8"))
+            cstate = d.sessionLoopState
+            ws.send('[5, \"OnJsonApiEvent\"]');
+        } catch(e){
+            console.log("Eine abfrage an Valorant hat ein Fehler geworfen.")
+             ws.close()
+        }
+        
+    })
+    
+    ws.on('message', async data => {
+        var pdata = JSON.parse(data)
+        if(typeof pdata[2] == "object" && pdata[2].eventType == "Update" && pdata[2].uri == "/chat/v4/presences" && pdata[2].data.presences[0].puuid == tokens.data.subject) {
+            var decode = JSON.parse(Buffer.from(pdata[2].data.presences[0].private, "base64").toString("utf-8"));
+            if(cstate != decode.sessionLoopSate) {
+                if(decode.sessionLoopState == "MENUS") {cstate = decode.sessionLoopState;console.log("State: MENUS");return io.emit("update", {state: "Menu", data: decode})}
+                if(decode.sessionLoopState == "PREGAME") {cstate = decode.sessionLoopState;console.log("State: PREGAME");return io.emit("update", {state: "PreGame", data: decode})}
+                if(decode.sessionLoopState == "INGAME") {cstate = decode.sessionLoopState;console.log("State: INGAME");return io.emit("update", {state: "Ingame", data: decode})}
+            }
+        }
+    });
+    
+    
+    ws.on('close', async data => {
+        console.log('Socket is closed. Reconnect will be attempted in 10 second.', data.reason);
+        ws.close()
+        io.emit("update", {state: "Menu"});
+        console.log("State: Close")
+        setTimeout(function() {
+            if(ws.readyState == 2 || ws.readyState == 3){
+                websocketConnect()
+           }
+        }, 10000);
+    });
+
+    ws.on('error', async data => {
+        console.error('Socket encountered error: ', data.message, 'Closing socket');
+            ws.close()
+    });
+
 }
 
 function fetchLogin() {
@@ -51,9 +119,19 @@ function fetchLogin() {
         var pw = matches[4]
         return {port: port, pw: pw}
     } else {
-        throw "VALORANT nicht offen"
+        console.log("Valorant ist nicht geöffnet, versuche es in 10 Sekunden erneut ...")
+        sleep(10000)
+        return fetchLogin()
     }
 }
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
 
 const errors = {
     404: {status: "404", message: "Not in expected state"},
@@ -122,40 +200,13 @@ fastify.get("/socket.io.min.js", async (req, res) => {
     res.type("text/javascript").send(buffer)
 })
 
-ws.on("open", async open => {
-    tokens = tokens == undefined ? await data() : tokens
-    var presence = await axios.get(`https://127.0.0.1:${wsdata.port}/chat/v4/presences`, {
-        headers: {
-            'Authorization': `Basic ${Buffer.from(`riot:${wsdata.pw}`, 'utf8').toString('base64')}`,
-            "user-agent": "ShooterGame/21 Windows/10.0.19042.1.768.64bit",
-            "X-Riot-ClientVersion": "release-02.03-shipping-8-521855",
-            "Content-Type": "application/json",
-            "rchat-blocking": "true"
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false,
-        })
-    }).catch(error => {return error})
-    var f = presence.data.presences.filter(item => item.puuid == tokens.data.subject)
-    var d = JSON.parse(Buffer.from(f[0].private, "base64").toString("utf-8"))
-    cstate = d.sessionLoopState
-    ws.send('[5, \"OnJsonApiEvent\"]');
-})
 
-ws.on('message', async data => {
-    var pdata = JSON.parse(data)
-    if(typeof pdata[2] == "object" && pdata[2].eventType == "Update" && pdata[2].uri == "/chat/v4/presences" && pdata[2].data.presences[0].puuid == tokens.data.subject) {
-        var decode = JSON.parse(Buffer.from(pdata[2].data.presences[0].private, "base64").toString("utf-8"));
-        if(cstate != decode.sessionLoopSate) {
-            if(decode.sessionLoopState == "MENUS") {cstate = decode.sessionLoopState;console.log("State: MENUS");return io.emit("update", {state: "Menu", data: decode})}
-            if(decode.sessionLoopState == "PREGAME") {cstate = decode.sessionLoopState;console.log("State: PREGAME");return io.emit("update", {state: "PreGame", data: decode})}
-            if(decode.sessionLoopState == "INGAME") {cstate = decode.sessionLoopState;console.log("State: INGAME");return io.emit("update", {state: "Ingame", data: decode})}
-        }
-    }
-});
 
 io.on("connection", async socket => {
     console.log("Connected")
+    socket.on('disconnect', () => {
+        console.log('Disconnected')
+    })
     var presence = await axios.get(`https://127.0.0.1:${wsdata.port}/chat/v4/presences`, {
         headers: {
             'Authorization': `Basic ${Buffer.from(`riot:${wsdata.pw}`, 'utf8').toString('base64')}`,
@@ -168,14 +219,25 @@ io.on("connection", async socket => {
             rejectUnauthorized: false,
         })
     }).catch(error => {return error})
-    var f = presence.data.presences.filter(item => item.puuid == tokens.data.subject)
+    if(presence instanceof Error){
+        console.log("Eine abfrage an Valorant hat ein Fehler geworfen.")
+        io.emit("initialize", {state: "Menu"});
+        console.log("State: Close")
+        return
+    }
+    try {
+          var f = presence.data.presences.filter(item => item.puuid == tokens.data.subject)
     var d = JSON.parse(Buffer.from(Buffer.from(f[0].private, "base64").toString("utf-8")).toString("utf-8"))
     if(d.sessionLoopState == "MENUS") {io.emit("initialize", {state: "Menu", data: d});console.log("State: MENUS")}
     if(d.sessionLoopState == "PREGAME") {io.emit("initialize", {state: "PreGame", data: d});console.log("State: PREGAME")}
     if(d.sessionLoopState == "INGAME") {io.emit("initialize", {state: "Ingame", data: d});console.log("State: INGAME")}
-    socket.on('disconnect', () => {
-        console.log('Disconnected')
-    })
+    } catch(e){
+        console.log("Eine abfrage an Valorant hat ein Fehler geworfen.")
+        io.emit("update", {state: "Menu"});
+        console.log("State: Close")
+        return
+    }
+  
 })
 
 fastify.listen(5000, '127.0.0.1', (err, address) => {console.log(`Online auf ${address}`)})
